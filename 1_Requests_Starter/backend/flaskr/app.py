10bad280b308a8a37c92f7a78c5aa2313ffac720
plainsight16 <64688744+plainsight16@users.noreply.github.com>
2022-06-13 13:56:49 +0100

    Completed endpoints and error-handling
    Endpoints:
        - GET books
        - Update rating
        - DELETE book
        - POST book
    Created error handling for 404, 400, 422

---
+++ 1_Requests_Starter/backend/flaskr/app.py
@@ -0,0 +1,153 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy  # , or_
+from flask_cors import CORS
+import random
+
+from models import setup_db, Book
+
+BOOKS_PER_SHELF = 8
+
+# @TODO: General Instructions
+#   - As you're creating endpoints, define them and then search for 'TODO' within the frontend to update the endpoints there.
+#     If you do not update the endpoints, the lab will not work - of no fault of your API code!
+#   - Make sure for each route that you're thinking through when to abort and with which kind of error
+#   - If you change any of the response body keys, make sure you update the frontend to correspond.
+
+
+# create and configure the app
+app = Flask(__name__)
+setup_db(app)
+CORS(app)
+
+# CORS Headers
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add(
+        "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+    )
+    response.headers.add(
+        "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+    )
+    return response
+
+
+def paginated_pages(request):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * BOOKS_PER_SHELF
+    end = start + BOOKS_PER_SHELF
+    books = Book.query.order_by(Book.id).all()
+    paginated_books = [book.format() for book in books]
+    return paginated_books[start:end]
+
+# TEST: When completed, the webpage will display books including title, author, and rating shown as stars
+
+
+@app.route('/')
+def books():
+    current_books = paginated_pages(request)
+    if len(current_books) == 0:
+        abort(404)
+    return jsonify({
+        "success": True,
+        "books": current_books,
+        "total_books": len(Book.query.all())
+    })
+
+
+@app.route('/books/<int:id>', methods=['PATCH'])
+def update_rating(id):
+    body = request.get_json()
+    try:
+        book = Book.query.get(id)
+
+        if "rating" in body:
+            rating = int(body.get('rating'))
+            book.rating = rating
+        book.update()
+    except:
+        abort(404)
+
+    return jsonify({
+        "success": True,
+        "id": id,
+        "books": paginated_pages(request)
+    })
+
+# TEST: When completed, you will be able to click on stars to update a book's rating and it will persist after refresh
+
+
+@app.route('/books/<int:id>', methods=["DELETE"])
+def delete_book(id):
+    try:
+        book = Book.query.filter(Book.id == id).one_or_none()
+        book.delete()
+    except:
+        abort(404)
+
+    return jsonify({
+        'success': True,
+        'deleted': id,
+        'books': paginated_pages(request),
+        'total_books': len(Book.query.all())
+    })
+# TEST: When completed, you will be able to delete a single book by clicking on the trashcan.
+
+
+# TEST: When completed, you will be able to a new book using the form. Try doing so from the last page of books.
+#       Your new book should show up immediately after you submit it at the end of the page.
+
+@app.route('/books', methods=['POST'])
+def create_new_book():
+    body = request.get_json()
+    if body == None:
+        abort(404)
+    new_title = body.get("title", None)
+    new_author = body.get("author", None)
+    new_rating = body.get("rating", None)
+
+    try:
+        book = Book(title=new_title, author=new_author, rating=new_rating)
+        book.insert()
+    except:
+        abort(422)
+
+    return jsonify({
+        "success": True,
+        "created": book.id,
+        "books": paginated_pages(request),
+        "total_books": len(Book.query.all())
+    })
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "Bad Request"
+    }), 422
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "Resource Not Found"
+    }), 404
+
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable request"
+    }), 422
+
+
+if __name__ == '__main__':
+    app.run()
